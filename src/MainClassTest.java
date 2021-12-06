9aabfd1160e2cd59769169885cc991c1dd28fb8b
lenachagina <hel0len53@gmail.com>
2021-12-06 17:47:14 +0300

    I. Введение в Java и JUnit. Тест 1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MainClassTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+
+    @Test
+    public void testGetLocalNumber() {
+
+        int expectedValue = 14;
+
+        int resultValue = this.getLocalNumber();
+        String errorText = "Expected: " + expectedValue + " actually: " + resultValue;
+        Assert.assertTrue(errorText, resultValue == expectedValue);
+    }
+
 }
