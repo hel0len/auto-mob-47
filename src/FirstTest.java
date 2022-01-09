532beb8b17e22525de33cb760f10c3ed301becd3
lenachagina <hel0len53@gmail.com>
2022-01-09 19:32:19 +0300

    Ex5: Тест: сохранение двух статей

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -391,6 +391,116 @@ public class FirstTest {
                 5);
     }
 
+    // Добавление двух статей и удаление одной из списка 'My lists'
+    @Test
+    public void saveTwoArticlesToMyList() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Не найдена строка поиска",
+                10);
+        String first_search_word = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                first_search_word,
+                "Ошибка ввода текста в строку поиска",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Java (programming language)')]"),
+                "В выдаче не найдена страница Java (programming language)",
+                10);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Не найдена кнопка 'More options'",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Не найдена кнопка 'Add to reading list' в выпадающем списке",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'GOT IT')]"),
+                "Не найдена кнопка 'Go it' в модалке",
+                10);
+        waitForElementAndClear(
+                By.xpath("//*[@resource-id='org.wikipedia:id/text_input']"),
+                "Не найдено поле ввода названия списка",
+                10);
+        String folder_name = "Learning programming";
+        waitForElementAndSendKeys(
+                By.xpath("//*[@resource-id='org.wikipedia:id/text_input']"),
+                folder_name,
+                "Не найдено поле ввода названия списка",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Не удалось нажатие кнопки 'OK' в окне создания списка",
+                10);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Не найдена кнопка закрытия статьи - 'x'",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Не найдена строка поиска",
+                10);
+        String second_search_word = "Appium";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                second_search_word,
+                "Ошибка ввода текста в строку поиска",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][contains(@text, '" +
+                        second_search_word + "')]"),
+                "В выдаче не найдена страница " + second_search_word,
+                15);
+        String old_second_article_title = waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text']"),
+                "Не найден заголовок страницы",
+                10)
+                .getText();
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Не найдена кнопка 'More options'",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Не найдена кнопка 'Add to reading list' в выпадающем списке",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/item_title'][@text='" + folder_name + "']"),
+                "Не найден созданный список при добавлении в него новой статьи",
+                10);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Не найдена кнопка закрытия статьи - 'x'",
+                10);
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Не найдена кнопка нижнего меню 'My lists'",
+                10);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/item_title'][@text='" + folder_name + "']"),
+                "Не найден созданный список с названием" + folder_name,
+                10);
+        swipeElementToLeft(
+                By.xpath("//*[contains(@text, 'Java (programming language)')]"),
+                "Не удалось удаление статьи из списка с помощью свайпа влево");
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + second_search_word + "']"),
+                "В списке не найдена НЕудаленная статья",
+                10);
+        String new_second_article_title = waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/view_page_title_text']"),
+                "Не найден заголовок страницы",
+                10)
+                .getText();
+        Assert.assertEquals(
+                "Заголовок статьи оставшейся в списке: " + new_second_article_title +
+                        " не совпадает с заголовком статьи, добавленной в список: " + old_second_article_title,
+                old_second_article_title,
+                new_second_article_title);
+    }
+
 
     // Ожидание присутствия элемента на странице с явным указанием таймаута
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSessions) {
